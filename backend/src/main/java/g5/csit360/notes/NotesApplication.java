07d2089266761ad86105219b1ecb09beded3ede5
Charles Dominic Hordista <cdominic51@gmail.com>
2025-09-24 19:33:28 +0800

    Connect springboot to database
    Backend update
    - Add print message when springboot runs successfully
    - Modify application.properties to connect to database

--- backend/src/main/java/g5/csit360/notes/NotesApplication.java
+++ backend/src/main/java/g5/csit360/notes/NotesApplication.java
@@ -8,6 +8,7 @@ public class NotesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotesApplication.class, args);
+		System.out.println("MUNCHKIN NOTES: Ze blutooth device has connected successfully.");
 	}
 
 }
